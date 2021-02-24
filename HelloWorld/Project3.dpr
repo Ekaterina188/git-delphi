0a6163a70644ef6052ce0d10850048df2912c179
kisamur.e@ya.ru <Ekaterina188>
2021-02-24 08:47:39 +0300

    fthf

---
+++ HelloWorld/Project3.dpr
@@ -0,0 +1,14 @@
+program Project3;
+
+uses
+  Vcl.Forms,
+  HelloWorld in 'HelloWorld.pas' {Form5};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm5, Form5);
+  Application.Run;
+end.
